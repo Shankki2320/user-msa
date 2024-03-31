edc87e82e5787da7e54ac43225c9dba1afaa85bf
shankki2320 <$hanKKi@2320>
2024-03-31 13:35:37 +0530

    Initial commit

---
+++ src/main/java/com/interview/usermsa/UserMsaApplication.java
@@ -0,0 +1,13 @@
+package com.interview.usermsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMsaApplication.class, args);
+    }
+
+}
